6529fe75ae2dd8b93e02bca630eb3aad80a186ce
JoãoPedroBoneco <joao.pedro.13dec06@gmail.com>
2025-02-05 10:16:27 +0000

    Update GameFrame.java

--- src/ui/GameFrame.java
+++ src/ui/GameFrame.java
@@ -1,5 +1,13 @@
 package src.ui;
 
 public class GameFrame {
-    
+    public GameFrame(){
+        this.setTitle("Bomberman Game");
+
+        this.setdefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.setResizable(false);
+            this.setContentPane(new GamePanel());
+            this.pack();
+            this.setLocationRelativeTo(null); // Centraliza a janela.
+    }
 }
