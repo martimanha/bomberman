e2f0063a5dc4fff343ea0b45e0c7651750a82626
m <martimanha7@gmail.com>
2025-02-12 08:25:33 +0000

    auihfojiº

--- src/main/Launcher.java
+++
@@ -1,9 +0,0 @@
-package src.main;
-import src.ui.GameFrame;
-
-public class Launcher{
-    public static void main(String[] args){
-        GameFrame gameFrame = new GameFrame();
-        gameFrame.setVisible(true);
-    }
-}
